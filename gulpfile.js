2ccb377aafebd9217b81e3e7b22a61cacb6b9d7b
Aaron Crowder <aaron.crowder@gmail.com>
2016-01-02 17:25:27 -0700

    Working on adding font-awesome
    Along with that I need to add more features to my gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    del = require('del');
 
 var paths = {
-    packages: ['node_modules/bootstrap/dist/**/*']
+    packages: [
+        { src: 'node_modules/bootstrap/dist/**/*', dest: 'bootstrap'},
+        'node_modules/font-awesome/css/**/*',
+        'node_modules/font-awesome/fonts/**/*'],
+    output: 'static'
 };
 
-gulp.task('copy', function(){
+gulp.task('copy', function() {
     return gulp.src(paths.packages)
-        .pipe(gulp.dest('static/packages'));
+        .pipe(gulp.dest(paths.output));
 });
 
-gulp.task('default', ['copy']);
+gulp.task('clean', function() {
+    return del([paths.output + '/*']);
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('copy');
+});
